fd4a9d3efe22993ad4aec9621aabc59f94752210
TimZel <107506944+TimZel@users.noreply.github.com>
2022-12-09 21:02:42 +0300

    Update SingleLinkedList.java

--- list/src/main/java/SingleLinkedList.java
+++ list/src/main/java/SingleLinkedList.java
@@ -6,23 +6,16 @@ public class SingleLinkedList implements MyList {
     }
     public int size() { //если лист пустой, вернёт - 0
         int size = 0; //счетчик
-        if (head != null) { //если лист не пустой, начинаю отсчет с 1
-            size = 1;
-            Node current = head; //получаю ссылку на 1й элемент
-            while (current.next != null) { //пока есть следующий элемент
-                current = current.next; //переключаю на след элемент
-                size++; //считаю количество элементов
-            }
+        for(Node current = head; current != null; current = current.next) {
+            size++; //считаю количество элементов
         }
         return size;
     }
-
     public void insertAfter(int position, int value) {  //вставить - значение после указанной позиции
         int index = 0; //счетчик позиции
         if(position >= 0 && position <= this.size()) { //если позиция элемента не 0 и лист не пустой
             Node current = head; //получаю ссылку на 1й элемент
-            while (index != position) { //пока счетчик не равен позиции
-                current = current.next; //перемещаю на след.элемент
+            for( ; index != position; current = current.next) { //пока счетчик не равен позиции - ищу след-й эл
                 index++; //увеличиваю счетчик
             }
             Node newNode = new Node(value);//создаю новый элемент с указанным значением
@@ -32,36 +25,31 @@ public class SingleLinkedList implements MyList {
     }
 
     int getIndex(int input) { //получить индекс по введенному значению
-        Node current = head; // получаю ссылку на 1й элемент
         int counter = 0; //счетчик - нуль
-        while(current != null) { //пока элемент существует
+        for(Node current = head; current != null; current = current.next) {
             if(current.data == input) { //если есть в списке
                 return counter; //возвращаю его индекс
             }
-            current = current.next; //переключаюсь на след-й
             counter++;//увеличиваю счетчик
         }
         return -1; //если нет, вернет -1
     }
+
     public int getElement(int index) {
-        Node current = head;
         int counter = 0; //счетчик - нуль
-        while(current != null) { //пока элемент существует - ищу
+        for(Node current = head; current != null; current = current.next) {
             if(counter == index) { //если счетчик и индекс совпадают
                 return current.data; //возвращаю его значение элемента
             }
-            current = current.next; //переключаюсь на след-й и ищу
             counter++;//увеличиваю счетчик
         }
         return -1; //если нет, вернет -1
     }
     public boolean contains(int key) { //содержит ли введенный элемент
-        Node current = head; //получаю ссылку на 1й элемент
-        while(current != null) { //пока элемент существует - ищу
+        for(Node current = head; current != null; current = current.next) {
             if (current.data == key) {
                 return true; //есть ли такое значение есть, возвращаю true
             }
-            current = current.next; //переключаюсь на след-й и ищу
         }
         return false; //иначе false
     }
@@ -74,16 +62,12 @@ public class SingleLinkedList implements MyList {
     public void addBack(int value){ //добавить сзади
         Node newnode = new Node(value); //создаю новый элемент
         Node current = head; //получаю ссылку на первый элемент
-        while(current.next!=null) { //перехожу в конец списка
-            current = current.next;
-        }
+        for( ; current.next != null; current = current.next) {}
         current.next = newnode; //добавляю в конец новую запись
     }
     void printList() { //печать списка
-        Node current = head;       //получаю ссылку на первый элемент
-        while (current != null) { //пока элемент существует
+        for(Node current = head; current != null; current = current.next) {
             System.out.print(current.data + " "); //печатаю его данные
-            current = current.next; //и переключаюсь на следующий
         }
     }
 
@@ -95,8 +79,7 @@ public class SingleLinkedList implements MyList {
             head = head.next;    //переключаюуказатель начала на второй элемент
             return;              //и выхожу
         }
-        Node current = head;       //иначе начинаю искать
-        while (current.next != null) {    //пока следующий элемент существует
+        for(Node current = head; current.next != null; ) {
             if (current.next.data == data) {  //проверяю следующий элемент
                 current.next = current.next.next;   //найденный элемент выкидываю
                 return;                 //и выхожу
@@ -106,3 +89,4 @@ public class SingleLinkedList implements MyList {
         }
     }
 }
+
